308f2dce9f62d31b87bb79e0bd127e3549587a34
Ammar Amjad <ammar_amjad@ymail.com>
2022-11-12 21:14:05 -0500

    1st

--- test.erl
+++ test.erl
@@ -2,26 +2,49 @@
 -author("Ammar").
 
 %% API
--export([run/1, actorReceive/1, actorSender/1]). 
+-export([run/1, supervisor/1, actor/0]). 
  
-actorSender(PID) ->
-    PID ! {s2r, "rumor"},
-    io:fwrite("~p Sender ~n", [self()]).
+% actorSender(PID) ->
+%     PID ! {s2r, "rumor"},
+%     io:fwrite("~p Sender ~n", [self()]).
 
-actorReceive(0) ->
-    io:fwrite("~p Terminating Receiver ~n", [self()]),
-    "all rumors are received";
-actorReceive(NumNodes) -> 
+% actorReceive(0) ->
+%     io:fwrite("~p Terminating Receiver ~n", [self()]),
+%     "all rumors are received";
+% actorReceive(NumNodes) -> 
+%     receive
+%         {s2r, Msg} ->
+%             io:fwrite("~p~n", [Msg]),
+%             actorReceive(NumNodes - 1)
+%     end.
+ 
+actor() -> 
     receive
-        {s2r, Msg} ->
-            io:fwrite("~p~n", [Msg]),
-            actorReceive(NumNodes - 1)
+        {lob, ListofNeighbors} ->
+            done
     end.
- 
 
+sendNeighborsToActors(ListofList, NumNodes, curridx) when curridx =< NumNodes ->
+    
+     
+
+    case NumNodes of
+            "full" -> done;
+            "2D" -> gossip2D;
+            "line" -> done;
+            "Imp2D" -> done
+        end
+    lists:nth(col, lists:nth(row, ListofList)),
+
+    lists:nth(1, lists:nth(1, ListofList)) ! NeighborList,
+    sendNeighborsToActors(ListofList, NumNodes, curridx + 1)
+    .
+
+supervisor(ListofList) -> 
+    sendNeighborsToActors(ListofList, NumNodes, 1).
 
 run(NumNodes) ->
-    PID = spawn(test, actorReceive, [NumNodes]),
+    PID = spawn(test, supervisor, [NumNodes]),
     % % Line
     % List = [spawn(test, actorSender, [PID]) || _ <- lists:seq(1, NumNodes)],
     % List print
@@ -30,7 +53,7 @@ run(NumNodes) ->
     % io:fwrite("~p here ~n ", [lists:nth(1, List)]),
 
     % 2D grid
-    ListofList = [[spawn(test, actorSender, [PID]) || _ <- lists:seq(1, NumNodes)] || _ <- lists:seq(1, NumNodes)],
+    ListofList = [[spawn(test, actor, [PID]) || _ <- lists:seq(1, NumNodes)] || _ <- lists:seq(1, NumNodes)],
     % ListofList print
     io:fwrite("~w~n", [ListofList]),
     % ListofList indexing
